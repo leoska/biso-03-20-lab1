d89ff38c2172a4c5ba30167835f70fd082c7da91
aermachkova <aermachkova@playflock.com>
2021-10-30 10:12:26 +0300

    first commit

---
+++ biso-03-20-lab1/biso-03-20-lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace biso_03_20_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Стэк, массив и пузырьковая сортировка
+            // Написать свой класс стэка с необходимыми методами для работы
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
